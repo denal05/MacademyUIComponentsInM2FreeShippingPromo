bc8f1071ecd12491d176d9175f499b1ff08c1f4e
Denis Aleksandrov <denal05@gmail.com>
2025-11-01 23:37:38 +0100

    Updated free-shipping-banner.js
    Create computed properties with Knockout -- in the initialize section, self.message becomes  ko.computed and its callback function returns one of the three messages defined in default.xml based on the state of the subtotal.

--- view/frontend/web/js/free-shipping-banner.js
+++ view/frontend/web/js/free-shipping-banner.js
@@ -1,26 +1,26 @@
 define([
 	'uiComponent',
-	'ko',
 	'Magento_Customer/js/customer-data',
-	'underscore'
+	'underscore',
+	'knockout'
 ], function(
 	Component,
-	ko,
 	customerData,
-	_
+	_,
+	ko
 ) {
 	'use strict';
 
 	return Component.extend({
 		defaults: {
-				message: '${ $.messageDefault }',
+				freeShippingThreshold: 100,
 				subtotal: 0.00,
 				template: 'Denal05_MacademyUIComponentsInM2FreeShippingPromo/free-shipping-banner',
 				tracks: {
-					message: true,
 					subtotal: true
 				}
 		},
+		
 		initialize: function() {
 			this._super();
 			var self = this;
@@ -41,6 +41,25 @@ define([
 				}
 			});
 			
+			self.message = ko.computed( function () {
+				// if subtotal == 0, return messageDefault
+				if (_.isUndefined(self.subtotal) || self.subtotal === 0) {
+					return self.messageDefault;
+				}
+
+				// if 0 > subtotal < 100, return messageItemsInCart
+				if (self.subtotal > 0 && self.subtotal < self.freeShippingThreshold) {
+					var subtotalRemaining = self.freeShippingThreshold - self.subtotal;
+					var formattedSubtotalRemaining = self.formatCurrency(subtotalRemaining);
+					return self.messageItemsInCart.replace('$XX.XX', formattedSubtotalRemaining);
+				}
+
+				// if subtotal >= 100, return messageFreeShipping
+				if (self.subtotal >= self.freeShippingThreshold) {
+					return self.messageFreeShipping;
+				}
+			});
+
 			// simulate the user updating the value
 			/*
 			window.setTimeout( function () {
@@ -48,6 +67,7 @@ define([
 			}, 2000);
 			*/
 		},
+		
 		formatCurrency: function(value) {
 			return '$' + value().toFixed(2);
 		}
