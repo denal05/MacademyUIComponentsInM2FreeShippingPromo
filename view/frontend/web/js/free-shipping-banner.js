61a8d537438f5fa4b3cb51a54f35ee14a6d0fc0f
Denis Aleksandrov <denal05@gmail.com>
2025-11-01 16:44:30 +0100

    Update free-shipping-banner.js
    Fixed a few typos.

--- view/frontend/web/js/free-shipping-banner.js
+++ view/frontend/web/js/free-shipping-banner.js
@@ -7,16 +7,16 @@ define([
 
 	return Component.extend({
 		defaults: {
-			message: 'Free Shipping Message - defaults property - Knockout template',
-            subtotal: 33.00, N
-			template: 'Denal05_MacademyUIComponentsInM2FreeShippingPromo/free-shipping-banner'
+    message: 'Free Shipping Message - defaults property - Knockout template',
+    subtotal: 33.00,
+    template: 'Denal05_MacademyUIComponentsInM2FreeShippingPromo/free-shipping-banner'
 		},
 		initialize: function() {
 			this._super();
 			console.log(this.message);
 		},
-        formatCurrency: function(value) {
-            return '$' + value.toFixed(2);
-        }
+		formatCurrency: function(value) {
+    return '$' + value.toFixed(2);
+		}
 	});
 });
