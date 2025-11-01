74c2ac1b5373adf79aecf59efc6bd075e9b87453
Denis Aleksandrov <denal05@gmail.com>
2025-11-01 18:51:23 +0100

    Updated free-shipping-banner.js
    Passed the 'subtotal' property value to ko.observable and simulated a user updating the value on the fronted via the JavaScript function window.setTimeout()

--- view/frontend/web/js/free-shipping-banner.js
+++ view/frontend/web/js/free-shipping-banner.js
@@ -1,22 +1,29 @@
 define([
-	'uiComponent'
+	'uiComponent',
+	'ko'
 ], function(
-	Component
+	Component,
+	ko
 ) {
 	'use strict';
 
 	return Component.extend({
 		defaults: {
     message: 'Free Shipping Message - defaults property - Knockout template',
-    subtotal: 33.00,
+    subtotal: ko.observable(33.00),
     template: 'Denal05_MacademyUIComponentsInM2FreeShippingPromo/free-shipping-banner'
 		},
 		initialize: function() {
 			this._super();
+			var self = this;
 			console.log(this.message);
+		
+		 window.setTimeout( function () {
+				self.subtotal(36.00);
+			}, 2000);
 		},
 		formatCurrency: function(value) {
-    return '$' + value.toFixed(2);
-		}
+    return '$' + value().toFixed(2);
+		} b
 	});
 });
